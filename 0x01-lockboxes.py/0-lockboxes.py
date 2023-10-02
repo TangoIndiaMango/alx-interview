f9c9136a2cea852f84e034218a86d6b1a9d69d14
Timilehin Aliyu <89034378+TangoIndiaMango@users.noreply.github.com>
2023-10-02 16:54:42 -0500

    redefeined function with doctype

---
+++ 0x01-lockboxes.py/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists representing locked boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
+    n = len(boxes)
+    visited_boxes = [False] * n
+    visited_boxes[0] = True
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if not visited_boxes[key]:
+                visited_boxes[key] = True
+                stack.append(key)
+
+    return all(visited_boxes)
