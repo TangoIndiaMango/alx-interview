a84e60f0896c2b46960c74426834edda7aacef0b
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-10-03 13:41:27 -0500

    make executable

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
@@ -9,31 +8,16 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, else False.
     """
-    if len(boxes[0]) == 0:
-        return (False)
-    keys = {0}
-    size = len(boxes)
-    visited = {0}
-    keys = keys.union(boxes[0])
-    while size > 0:
-        for i in keys:
-            if i in visited:
-                continue
-            keys = keys.union(boxes[i])
-            visited.add(i)
-        size -= 1
-    return len(keys) == len(boxes)
+    n = len(boxes)
+    visited_boxes = [False] * n
+    visited_boxes[0] = True
+    stack = [0]
 
-    # n = len(boxes)
-    # visited_boxes = [False] * n
-    # visited_boxes[0] = True
-    # stack = [0]
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if not visited_boxes[key]:
+                visited_boxes[key] = True
+                stack.append(key)
 
-    # while stack:
-    #     current_box = stack.pop()
-    #     for key in boxes[current_box]:
-    #         if not visited_boxes[key]:
-    #             visited_boxes[key] = True
-    #             stack.append(key)
-
-    # return all(visited_boxes)
+    return all(visited_boxes)
