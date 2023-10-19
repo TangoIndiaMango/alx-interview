b455a85577a6c57d5413c08b6020036af518a1a4
Timilehin Aliyu <89034378+TangoIndiaMango@users.noreply.github.com>
2023-10-19 16:13:00 -0500

    trailing white character

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,42 +6,42 @@ and prints stats to stdout
 
 import sys
 
-# status codes dictionary
+# Status codes dictionary
 status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                      '404': 0, '405': 0, '500': 0}
 
 total_size = 0
-count = 0  # line counter
+count = 0  # Line counter
 
 try:
     for line in sys.stdin:
+        line = line.strip()  # Remove trailing newline character
         line_list = line.split(" ")
 
         if len(line_list) > 4:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
 
-            # check if the status code passed exists then
-            # increment its count
-            if status_code in status_codes_dict.keys():
+            # Check if the status code exists, then increment its count
+            if status_code in status_codes_dict:
                 status_codes_dict[status_code] += 1
 
-            # increment total size
+            # Increment the total size
             total_size += file_size
 
-            # update line counter
+            # Update the line counter
             count += 1
 
         if count == 10:
-            count = 0  # reset counter
+            count = 0  # Reset the counter
             print('File size: {}'.format(total_size))
 
-            # print out status code counts
+            # Print out status code counts
             for key, value in sorted(status_codes_dict.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
-except Exception as err:
+except KeyboardInterrupt:
     pass
 
 finally:
