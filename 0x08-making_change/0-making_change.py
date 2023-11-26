373f6be7913c8c1e71ecb2b29436d3b6c4bec788
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-11-26 07:02:32 -0800

    this commit will push making change function

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!usr/bin/python3
+"""
+Function to determine fewest number
+of coins needed to meet a given total
+"""
+
+def makeChange(coins, total):
+    """
+    args: coins -> List of positive integers
+          total -> A whole value
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    coin_count = 0
+    remaining_total = total
+
+    for coin in coins:
+        num_coins = remaining_total // coin
+
+        coin_count += num_coins
+        remaining_total -= num_coins * coin
+    return coin_count if remaining_total == 0 else -1
