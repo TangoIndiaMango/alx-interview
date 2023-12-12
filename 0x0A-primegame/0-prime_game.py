85095e061bd6d63464d0313c44b0bbed62ceb25e
Timilehin Aliyu <89034378+TangoIndiaMango@users.noreply.github.com>
2023-12-12 07:25:21 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,21 +5,24 @@ from two people who take turns in
 choosing a prime number
 """
 
+
 def remove_multiple_prime_numbers(ls, n):
     """
     ls -> a list of numbers
     n -> valid integer
+    Remove multiples of n in the list ls.
     """
     for i in range(2, len(ls)):
         try:
             ls[n * i] = 0
-        except (ValueError, IndexError):
+        except IndexError:
             break
 
 def isWinner(x, nums):
     """
     x -> num of rounds
     nums -> list of valid integers
+    Determines the winner of the game based on prime numbers chosen.
     """
 
     if x <= 0 or nums is None:
@@ -30,13 +33,13 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    numbers = [1 for i in range(sorted(nums)[-1] + 1)]
+    numbers = [1 for _ in range(max(nums) + 1)]
     numbers[0], numbers[1] = 0, 0
     for i in range(len(numbers)):
         remove_multiple_prime_numbers(numbers, i)
 
     for i in nums:
-        if sum(numbers[0:i+1]) % 2 == 0:
+        if sum(numbers[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
