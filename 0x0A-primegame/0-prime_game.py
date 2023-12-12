c018aa68c7fb3a82fcaf1967f9c21230ffabf59d
Timilehin Aliyu <89034378+TangoIndiaMango@users.noreply.github.com>
2023-12-12 07:27:03 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
-"""
-This function determines a winner
-from two people who take turns in
-choosing a prime number
-"""
+"""0. Prime Game"""
 
-
-def remove_multiple_prime_numbers(ls, n):
-    """
-    ls -> a list of numbers
-    n -> valid integer
-    Remove multiples of n in the list ls.
-    """
-    for i in range(2, len(ls)):
-        try:
-            ls[n * i] = 0
-        except Exception as e:
-            break
 
 def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
     """
-    x -> num of rounds
-    nums -> list of valid integers
-    Determines the winner of the game based on prime numbers chosen.
-    """
-
     if x <= 0 or nums is None:
         return None
     if x != len(nums):
@@ -33,13 +14,13 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    numbers = [1 for _ in range(max(nums) + 1)]
-    numbers[0], numbers[1] = 0, 0
-    for i in range(len(numbers)):
-        remove_multiple_prime_numbers(numbers, i)
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
 
     for i in nums:
-        if sum(numbers[0:i + 1]) % 2 == 0:
+        if sum(a[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
@@ -47,5 +28,15 @@ def isWinner(x, nums):
         return "Ben"
     if maria > ben:
         return "Maria"
-    else:
-        return None
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
